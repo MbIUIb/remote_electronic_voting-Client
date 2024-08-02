c714b462df8ece6397110a01ea5eaa09e49955b9
MbIUIb <kirill.chezlov@gmail.com>
2024-08-02 21:00:47 +0300

    registration and authentication realization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 HOST = 'localhost'
@@ -5,9 +6,27 @@ PORT = 9999
 
 socket = socket.create_connection((HOST, PORT))
 
-msg = "hello world123!!!"
-socket.send(msg.encode())
+registration_stmt = {"request": "registration",
+                     "firstname": "mbiuib1",
+                     "lastname": "mbiuib",
+                     "password": "mbiuib123"}
+json_data = json.dumps(registration_stmt)
+socket.send(json_data.encode())
 
 data = socket.recv(1024)
 print(data.decode())
+
+
+authentication_stmt = {"request": "authentication",
+                       "firstname": "mbiuib1",
+                       "lastname": "mbiuib",
+                       "password": "mbiuib123"}
+json_data = json.dumps(authentication_stmt)
+socket.send(json_data.encode())
+
+data = socket.recv(1024)
+print(data.decode())
+
+socket.send("data".encode())
+
 socket.close()
